28413dd9df73e9d0a91b3389f4687e83d0678c27
The "Weird" Midas <alcatralawsilva@gmail.com>
2025-09-20 23:10:20 -0300

    using corectl as our main hotplug is beneficial

---
+++ config/c0_c4_c6.sh
@@ -0,0 +1,30 @@
+#!/system/bin/sh
+#
+# Copyright (C) 2025 Weird Midas
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Runonce after boot, to speed up the transition of power modes in powercfg
+
+apply_once() {
+    set_corectl_param "enable" "0:1 4:1 6:1"
+    set_corectl_param "min_cpus" "0:4 4:2 6:0"
+    set_corectl_param "busy_down_thres" "0:20 4:40 6:40"
+    set_corectl_param "busy_up_thres" "0:40 4:70 6:70"
+    set_corectl_param "offline_delay_ms" "0:100 4:100 6:50"
+    set_corectl_param "nr_prev_assist_thresh" "6:1"
+    set_corectl_param "task_thres" "4:3"
+}
+
+apply_once
---
+++ config/c0_c4_c7.sh
@@ -0,0 +1,30 @@
+#!/system/bin/sh
+#
+# Copyright (C) 2025 Weird Midas
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Runonce after boot, to speed up the transition of power modes in powercfg
+
+apply_once() {
+    set_corectl_param "enable" "0:1 4:1 7:1"
+    set_corectl_param "min_cpus" "0:4 4:2 7:0"
+    set_corectl_param "busy_down_thres" "0:20 4:40 7:40"
+    set_corectl_param "busy_up_thres" "0:40 4:70 7:60"
+    set_corectl_param "offline_delay_ms" "0:100 4:100 7:50"
+    set_corectl_param "nr_prev_assist_thresh" "7:1"
+    set_corectl_param "task_thres" "4:3"
+}
+
+apply_once
